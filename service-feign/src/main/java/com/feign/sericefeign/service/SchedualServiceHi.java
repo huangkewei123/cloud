05e3b5859d73b57d0f547d4e2aa17619318ea004
huangkewei123 <huangkewei>
2019-09-05 21:22:47 +0800

    升级，取消熔断器

--- service-feign/src/main/java/com/feign/sericefeign/service/SchedualServiceHi.java
+++ service-feign/src/main/java/com/feign/sericefeign/service/SchedualServiceHi.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
+//,fallback = SchedualServiceHiHystric.class
+@FeignClient(value = "service-hi")
 public interface SchedualServiceHi {
 
     @RequestMapping(value = "/user/hi",method = RequestMethod.GET)
